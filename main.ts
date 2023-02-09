986070c8191e052832cf1ea60b645d0853232016
saloom999 <124618459+saloom999@users.noreply.github.com>
2023-02-09 11:46:15 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+    }
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
